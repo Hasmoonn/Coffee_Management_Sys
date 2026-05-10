f287205a5babd7e30376dcfb08b18323546f7f87
Hasmoonn <mohamedhasmoon175@gmail.com>
2026-05-11 00:32:47 +0530

    modification

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,7 +1,4 @@
-
+// api/index.ts
 import app from '../src/app'
-import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req as any, res as any)
-}
+export default app
